ccaee3f913e56f938cf7aceec0594b14531edd1c
matheus2049alves <mathii10costa@gmail.com>
2024-05-19 23:47:30 -0300

    faz o perfil do usuário

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import {Details} from './pages/Details'
 import { Home } from './pages/Home'
 import { SignIn } from './pages/SignIn'
-import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 import { ThemeProvider} from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyle from "./styles/global"
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}> 
       <GlobalStyle/>
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,62 @@
+import { Container, Form,Avatar } from "./style";
+import {FiArrowLeft, FiUser,FiMail,FiLock,FiCamera} from "react-icons/fi"
+import { Input } from "../../components/Input";
+import { Button } from "../../components/button";
+
+
+export function Profile(){
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft/>
+        </a>
+      </header>
+      <Form>
+        
+        <Avatar>
+          <img 
+            src="https://github.com/matheus2049alves.png" 
+            alt="foto de usuário" 
+          />
+          <label
+            htmlFor="avatar">
+               <FiCamera/>
+               <input 
+                id="avatar"
+                type="file"
+            />
+           </label>
+         
+          
+        </Avatar>
+        
+        <Input 
+        placeholder = "Nome"
+        type = "text"
+        icon={FiUser}/>
+      
+        <Input 
+        placeholder = "E-mail"
+        type = "text"
+        icon={FiMail}/>
+     
+        <Input 
+        placeholder = "Senha Atual"
+        type = "password"
+        icon={FiLock}/>
+     
+        <Input 
+        placeholder = "Nova Senha"
+        type = "password"
+        icon={FiLock}/>
+        
+        <Button title={"Salvar"}/>
+
+      </Form>
+
+    
+    </Container>
+
+  )
+}
