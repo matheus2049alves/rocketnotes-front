c6bb30b16fe3d2caf908477ea9e25d345b32f989
matheus2049alves <mathii10costa@gmail.com>
2024-05-31 19:41:53 -0300

    hook de autenticação

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -2,11 +2,24 @@ import { Container, Form, BackgroundImg } from "./style";
 import { Button } from "../../components/button";
 import { Input } from "../../components/Input";
 import { Link } from "react-router-dom";
-import {FiLogIn,FiMail,FiLock} from "react-icons/fi"
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi"
+
+import { useAuth } from "../../hook/auth";
+import { useState } from "react";
 
 
-export function SignIn(){
-  return(
+export function SignIn() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassWord] = useState("")
+
+  const {signIn} = useAuth()
+
+  function handleSignIn(){
+    signIn({email, password})
+  }
+
+  return (
     <Container>
       <Form>
         <h1>Rocket Notes</h1>
@@ -14,26 +27,28 @@ export function SignIn(){
         <h2>Vem com a gente</h2>
 
         <Input
-          placeholder = "E-mail"
-          type = "text"
+          placeholder="E-mail"
+          type="text"
           icon={FiMail}
-         />
+          onChange = {e => setEmail(e.target.value)}
+        />
 
         <Input
-          placeholder = "Senha"
-          type = "password"
+          placeholder="Senha"
+          type="password"
           icon={FiLock}
-         />
+          onChange = {e => setPassWord(e.target.value)}
+        />
 
-        <Button title={"Entrar"}/>
+        <Button title={"Entrar"} onClick = {handleSignIn}/>
 
-        <Link to = "/register">
-          Criar Contar  
-        </Link> 
+        <Link to="/register">
+          Criar Contar
+        </Link>
 
       </Form>
-      <BackgroundImg/>
-      
+      <BackgroundImg />
+
     </Container>
   )
 }
